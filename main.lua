0231277359856ae41cd5b2f04e5c252f2c415902
vu1canic <40404096+vu1canic@users.noreply.github.com>
2022-01-04 01:38:02 +0100

    maybe this time

--- main.lua
+++ main.lua
@@ -74,40 +74,46 @@ end)
 Hook_AddListener("ConnectedToServer", "Connected_server_test", function(jsonData)
 
 	get_nicks()
+    DebugPrint("getting nicks as client")
 end)
 
 Hook_AddListener("TDMP_PlayerDamaged", "PrintStuffWhenDamageSomebody", function(jsonData)
 	local data = json.decode(jsonData)
 
 	DebugPrint(data.ID .. " was damaged in " .. data.Hit .. "(" .. data.Damage .. ")")
-    print(jsonData)
+    --print(jsonData)
 
 
 end)
--- tick function just gets the client nickname for now
 
 function tick()
     TDMP_Hook_Queue()
 end
 
 function get_nicks()
-    if #TDMP_GetPlayers() ~= 1 then
-        for i, ply in ipairs(TDMP_GetPlayers()) do
-            nicks[ply.id] = ply.nick
-            DebugPrint("TDMP id: "..ply.id.." nick: "..nicks[ply.id])
-            if TDMP_IsMe(ply.id) then
-                --clientNick = ply.nick
-                --client_steamId = ply.steamId
-                client_id = ply.id
-                break
-            end
+    for i, ply in ipairs(TDMP_GetPlayers()) do
+        nicks[ply.id] = ply.nick
+        DebugPrint("TDMP id: "..ply.id.." nick: "..nicks[ply.id])
+        if TDMP_IsMe(ply.id) then
+            --clientNick = ply.nick
+            --client_steamId = ply.steamId
+            client_id = ply.id
+            break
         end
     end
 end
 
-function init()
+
+if TDMP_IsServer() then
     get_nicks()
+    DebugPrint("as server")
 end
+-- function init()
+--     if TDMP_IsServer() then 
+--         get_nicks() 
+--         DebugPrint("getting nicks as server")
+--     end
+-- end
 
 function update(dt)
 end
